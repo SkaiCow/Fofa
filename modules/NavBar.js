0c4dd5d82bd9b7fe6042ce6a160d148a7137a1af
SkaiCow <WillALasater@gmail.com>
2021-05-20 02:40:06 -0500

    sexy nav menu

--- modules/NavBar.js
+++ modules/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity, Animated } from 'react-native'
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity, Animated, TouchableWithoutFeedback } from 'react-native'
 import Bars from '../files/3bar.svg'
 
 class NavBar extends React.Component
@@ -7,17 +7,21 @@ class NavBar extends React.Component
   constructor(props) {
     super(props)
 
-    this.state = {menuPos: new Animated.Value(menuWidth * -1)}
-  }
-
-  onPress = ()=>{
-    this.SlideIn()
+    this.state = {menuPos: new Animated.Value(0)}
   }
 
   SlideIn = ()=>{
     Animated.timing(this.state.menuPos, {
+      toValue: menuWidth,
+      duration: 100,
+      useNativeDriver: true,
+    }).start()
+  }
+  SlideOut = ()=>{
+    Animated.timing(this.state.menuPos, {
       toValue: 0,
-      duration: 100
+      duration: 100,
+      useNativeDriver: true,
     }).start()
   }
 
@@ -26,20 +30,29 @@ class NavBar extends React.Component
     return(
       <View>
         <View style={styles.NavBarConatiner}>
-          <TouchableOpacity onPress={this.onPress}>
+          <TouchableOpacity onPress={this.SlideIn}>
             <Bars width = "50" height="50"/>
           </TouchableOpacity>
         </View>
-        <Animated.View style={[styles.MenuWindow,{
-          left: this.state.menuPos
+        <Animated.View style={[styles.MenuContainer,{
+          transform: [{translateX: this.state.menuPos}]
         }]}>
+          <View style={styles.MenuWindow}>
+            <View style={styles.MenuSpacer}/>
+            <View style={styles.MenuButton}><Text style={styles.MenuButtonText}>Home</Text></View>
+            <View style={styles.MenuButton}><Text style={styles.MenuButtonText}>All Stats</Text></View>
+            <View style={styles.MenuButton}><Text style={styles.MenuButtonText}>Calender</Text></View>
+          </View>
+          <TouchableWithoutFeedback onPress={this.SlideOut}>
+            <View style={styles.MenuSidePanel}/>
+          </TouchableWithoutFeedback>
         </Animated.View>
       </View>
     )
   }
 }
 
-const menuWidth = Dimensions.get('window').width * .8;
+const menuWidth = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
   NavBarConatiner:{
@@ -50,18 +63,49 @@ const styles = StyleSheet.create({
     paddingLeft: 20,
     zIndex: 100,
     borderColor: "#E1B03D",
-    borderBottomWidth: 3
+    borderBottomWidth: 2
+  },
+
+  MenuContainer:
+  {
+    position: 'absolute',
+    top: 0,
+    left: menuWidth * -1,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+    zIndex: 100,
+    flexDirection: 'row',
   },
   MenuWindow:
   {
-    position: "absolute",
-    top: 0,
+    position: "relative",
     backgroundColor: '#629677',
-    borderRightWidth: 3,
+    borderRightWidth: 2,
     borderColor: "#E1B03D",
-    width: menuWidth,
-    height: Dimensions.get('window').height,
-    zIndex: 100,
+    flex:4,
+    height: '100%',
+  },
+  MenuSidePanel:{
+    position: 'relative',
+    height: '100%',
+    flex:1,
+  },
+  MenuSpacer:{
+    width: '100%',
+    height: 75,
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0,0,0,.2)',
+  },
+  MenuButton:{
+    width: '100%',
+    height: 50,
+    paddingLeft: 10,
+    justifyContent: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0,0,0,.2)',
+  },
+  MenuButtonText:{
+    fontSize: 30,
   }
 })
 
